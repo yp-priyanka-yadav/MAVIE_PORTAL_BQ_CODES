07bc670fb508b7805c11113e77fb5c505d1c144f
yp-priyanka-yadav <priyanka.yadav@mavie.care>
2026-06-15 09:56:22 +0200

    Update consultation_time.sql

--- consultation_time.sql
+++ consultation_time.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `mavie-platform-production.cdp_events_aggregate.cdp_events_avergae_duration_days` AS
+CREATE OR REPLACE TABLE `mavie-platform-production.cdp_events_aggregate.cdp_events_avergae_duration_days_portals_portal` AS
 SELECT
   ROUND(
     AVG(
@@ -26,7 +26,9 @@ WHERE unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.event_name =
 and app_id = 'portals-portal';
 
 
+--- preferred timeslots for telemedicine booking
 
+CREATE OR REPLACE TABLE `mavie-platform-production.cdp_events_aggregate.cdp_events_preferred_time_slots_portals_portal` AS
 WITH appointments AS (
   SELECT
     EXTRACT(
@@ -48,6 +50,7 @@ WITH appointments AS (
 
   FROM `mavie-platform-production.cdp_events_dataset_production.cdp_events_production`
   WHERE unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.event_name = 'booking_completed'
+  and  app_id = 'portals_portal'
 )
 
 SELECT
@@ -65,61 +68,3 @@ SELECT
 FROM appointments
 GROUP BY appointment_hour, day_type
 ORDER BY appointment_hour, day_type;
-
-
-
-WITH appointments AS (
-  SELECT
-    EXTRACT(
-      HOUR FROM PARSE_TIME(
-        '%H:%M',
-        unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.time_selected
-      )
-    ) AS appointment_hour
-  FROM `mavie-platform-production.cdp_events_dataset_production.cdp_events_production`
-  WHERE unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.event_name = 'booking_completed'
-)
-
-SELECT
-  CONCAT(
-    CAST(appointment_hour AS STRING),
-    '-',
-    CAST(appointment_hour + 1 AS STRING)
-  ) AS time_slot,
-  COUNT(*) AS bookings,
-  ROUND(
-    100 * COUNT(*) / SUM(COUNT(*)) OVER (),
-    2
-  ) AS percentage
-FROM appointments
-GROUP BY appointment_hour
-ORDER BY appointment_hour;
-
-
-
-
-WITH appointments AS (
-  SELECT
-    CASE
-      WHEN EXTRACT(
-        DAYOFWEEK FROM PARSE_DATE(
-          '%Y-%m-%d',
-          unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.date_selected
-        )
-      ) IN (1,7)
-      THEN 'Weekend'
-      ELSE 'Weekday'
-    END AS day_type
-  FROM `mavie-platform-production.cdp_events_dataset_production.cdp_events_production`
-  WHERE unstruct_event_care_mavie_portal_telemed_booking_flow_event_3.event_name = 'booking_completed'
-)
-
-SELECT
-  day_type,
-  COUNT(*) AS bookings,
-  ROUND(
-    100 * COUNT(*) / SUM(COUNT(*)) OVER (),
-    2
-  ) AS percentage
-FROM appointments
-GROUP BY day_type;
