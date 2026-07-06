e70d534c563d9d9123b8811a68533d6f6071b8ad
yp-priyanka-yadav <priyanka.yadav@mavie.care>
2026-07-06 10:42:14 +0200

    Update Hasura_data.sql

--- Hasura_data.sql
+++ Hasura_data.sql
@@ -1,4 +1,4 @@
------------ consultation_data
+----------- consultation_data 
 with base_data as
 (
   SELECT *,
@@ -20,12 +20,13 @@ consultations as
 select * from consultations;
 
 
------------ feedback_data
+----------- consultation as requested
 
 
-with base_data as
+with consultations as
 (
   SELECT *,
+    DATE(created_at) as event_date,
     case when specialization_key like '%optiweight%' or specialization_key like '%dietician%' then 'optiweight'
     when specialization_key like '%assistive%' then 'assistive' 
     when specialization_key like '%bipa%' then 'bipa'
@@ -34,11 +35,92 @@ with base_data as
     end as telemed_source
      FROM `global-grammar-425610-k5.telemed_export_dataset_staging.consultations` 
 ),
-consultations as
-(
-  select DATE(created_at) as event_date, telemed_source,
-  count(distinct consultation_id_from_consultation_table) as consultations
-  from base_data
-  group by event_date, telemed_source
+consultations_assistive as (
+SELECT
+    telemed_source,
+    COUNT(DISTINCT CASE
+        WHEN event_date BETWEEN
+             DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 7 DAY)
+             AND DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 1 DAY)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations,
+    COUNT(DISTINCT CASE
+        WHEN event_date >= DATE_TRUNC(CURRENT_DATE(), YEAR)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations_this_year  
+FROM consultations 
+where telemed_source = 'assistive' GROUP BY telemed_source
+),
+consultations_standarad as (
+SELECT
+    telemed_source,
+    COUNT(DISTINCT CASE
+        WHEN event_date BETWEEN
+             DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 7 DAY)
+             AND DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 1 DAY)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations,
+    COUNT(DISTINCT CASE
+        WHEN event_date >= DATE_TRUNC(CURRENT_DATE(), YEAR)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations_this_year  
+FROM consultations 
+where telemed_source = 'general' GROUP BY telemed_source
+),
+consultations_optiweight as (
+SELECT
+    telemed_source,
+    COUNT(DISTINCT CASE
+        WHEN event_date BETWEEN
+             DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 7 DAY)
+             AND DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 1 DAY)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations,
+    COUNT(DISTINCT CASE
+        WHEN event_date >= DATE_TRUNC(CURRENT_DATE(), YEAR)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations_this_year  
+FROM consultations 
+where telemed_source = 'optiweight' GROUP BY telemed_source
+),
+consultations_bipa as (
+SELECT
+    telemed_source,
+    COUNT(DISTINCT CASE
+        WHEN event_date BETWEEN
+             DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 7 DAY)
+             AND DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 1 DAY)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations,
+    COUNT(DISTINCT CASE
+        WHEN event_date >= DATE_TRUNC(CURRENT_DATE(), YEAR)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations_this_year  
+FROM consultations 
+where telemed_source = 'bipa' GROUP BY telemed_source
+),
+consultations_uniqa as (
+SELECT
+    telemed_source,
+    COUNT(DISTINCT CASE
+        WHEN event_date BETWEEN
+             DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 7 DAY)
+             AND DATE_SUB(DATE_TRUNC(CURRENT_DATE(), WEEK(MONDAY)), INTERVAL 1 DAY)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations,
+    COUNT(DISTINCT CASE
+        WHEN event_date >= DATE_TRUNC(CURRENT_DATE(), YEAR)
+        THEN consultation_id_from_consultation_table
+    END) AS consultations_this_year  
+FROM consultations 
+where telemed_source = 'uniqa' GROUP BY telemed_source
 )
-select * from consultations;
+select * from consultations_assistive
+union all
+select * from consultations_standarad
+union all
+select * from consultations_optiweight
+union all
+select * from consultations_uniqa
+union all
+select * from consultations_bipa;
